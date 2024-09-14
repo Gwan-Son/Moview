23d41eb068bdb8801662cfc755327eae4bf45fd6
GwanSon <id147369@naver.com>
2024-09-14 17:40:25 +0900

    feat: 영화 데이터 불러오기 확인

--- Moview/ContentView.swift
+++ Moview/ContentView.swift
@@ -8,12 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var viewModel = MovieViewModel()
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ForEach(viewModel.movies) { movie in
+                Text(movie.title)
+            }
         }
         .padding()
     }
