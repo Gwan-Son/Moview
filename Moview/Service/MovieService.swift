fdaaa18b986fe5d1e8567646f3c67c9f35f196d3
GwanSon <id147369@naver.com>
2024-09-14 17:41:48 +0900

    feat: Alamofire를 통해 API 호출

---
+++ Moview/Service/MovieService.swift
@@ -0,0 +1,39 @@
+//
+//  MovieService.swift
+//  Moview
+//
+//  Created by 심관혁 on 9/14/24.
+//
+
+import Foundation
+import Alamofire
+
+class MovieService {
+    let apiKey = Bundle.main.infoDictionary?["APIKEY"] as! String
+    
+    func getMovie(completion: @escaping (MovieResponse?) -> Void) {
+        let url = "https://api.themoviedb.org/3/discover/movie"
+        
+        print("apiKey: \(apiKey)")
+        
+        let params: [String: String] = [
+            "api_key": apiKey,
+            "include_adult": "false",
+            "include_video": "false",
+            "language": "ko-KR",
+            "page": "1",
+            "sort_by": "popularity.desc"
+        ]
+        
+        AF.request(url, method: .get, parameters: params, encoding: URLEncoding.default)
+            .responseDecodable(of: MovieResponse.self) { response in
+            switch response.result {
+            case .success(let response):
+                completion(response)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+}
