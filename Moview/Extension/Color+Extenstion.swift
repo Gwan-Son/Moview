d4b9ab9df0f423cb96237e4cc90c84901960654d
GwanSon <id147369@naver.com>
2024-09-18 15:44:53 +0900

    feat: hex코드로 사용할 수 있는 컬러 익스텐션 추가

--- Moview/Extension/Color+Extenstion.swift
+++ Moview/Extension/Color+Extenstion.swift
@@ -15,4 +15,11 @@ extension Color {
             blue: .random(in: 0...1)
         )
     }
+    init(hex: Int, opacity: Double = 1.0) {
+        let red = Double((hex >> 16) & 0xff) / 255
+        let green = Double((hex >> 8) & 0xff) / 255
+        let blue = Double((hex >> 0) & 0xff) / 255
+        
+        self.init(.sRGB, red: red, green: green, blue: blue, opacity: opacity)
+    }
 }
