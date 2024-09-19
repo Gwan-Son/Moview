d0620f138aacf7a59c30f0d5481a4ebb9410eb39
GwanSon <id147369@naver.com>
2024-09-19 15:04:06 +0900

    feat: Refactoring

---
+++ Moview/MovieView/GenreCardView.swift
@@ -0,0 +1,41 @@
+//
+//  CategoryCardView.swift
+//  Moview
+//
+//  Created by 심관혁 on 9/18/24.
+//
+
+import SwiftUI
+
+struct GenreCardView: View {
+    let genres = Genres.allGenres
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            LazyHStack {
+                ForEach(genres, id: \.1) { genre in
+                    NavigationLink(destination: GenreView(genre: genre)) {
+                        ZStack {
+                            Rectangle()
+                                .frame(width: 100, height: 60)
+                                .foregroundColor(Color(hex: 0xE5E5E5))
+                                .cornerRadius(10)
+                            Text(genre.0)
+                                .font(.system(size: 16))
+                                .bold()
+                                .foregroundColor(.black)
+                        }
+                    }
+                }
+            }
+        }
+        .frame(height: 70)
+        .padding(.leading, 15)
+        .padding(.bottom, 10)
+    }
+}
+
+
+#Preview {
+    GenreCardView()
+}
--- Moview/MovieView/MovieSlideView.swift
+++ Moview/MovieView/MovieSlideView.swift
@@ -17,25 +17,9 @@ struct MovieSlideView: View {
         ScrollView(.horizontal, showsIndicators: false) {
             LazyHStack(spacing: 20) {
                 ForEach((type != 0) ? viewModel.nowPlayingMovies : viewModel.popularMovies) { movie in
-                    ZStack(alignment: .leading) {
-                        VStack(spacing: 10) {
-                            KFImage(URL(string: movie.poster_path))
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 100)
-                                .shadow(radius: 10)
-                            
-                            Text(movie.title)
-                                .font(.system(size: 15))
-                                .fontWeight(.bold)
-                                .lineLimit(1)
-                                .multilineTextAlignment(.center)
-                                .frame(width: 100)
-                                .padding(.top, 1)
-                        }
-                        CircularProgressView(progress: movie.vote_average)
-                            .padding(.top, 100)
-                            .padding(.leading, 5)
+                    NavigationLink(destination: DetailView(movie: movie)) {
+                        MoviePosterView(movie: movie)
+                            .foregroundColor(.black)
                     }
                 }
             }
