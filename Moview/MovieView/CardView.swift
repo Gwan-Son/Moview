45fcd8c22b9e7fc6b27a0e2b6770eab7308f8f2a
GwanSon <id147369@naver.com>
2024-09-19 15:03:25 +0900

    fix: 사용자가 직접 화면을 넘겼을 때 발생하는 오류 수정

--- Moview/MovieView/CardView.swift
+++ Moview/MovieView/CardView.swift
@@ -18,27 +18,30 @@ struct CardView: View {
             ForEach(Array(viewModel.popularMovies.prefix(4).enumerated()), id: \.element) { index, movie in
                 
                 VStack(alignment: .leading) {
-                    ZStack {
-                        KFImage(URL(string: movie.poster_path))
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .overlay(LinearGradient(gradient: Gradient(colors: [Color.clear, Color.black.opacity(0.35)]), startPoint: .top, endPoint: .bottom))
-                        
-                        VStack(alignment: .leading, spacing: 10) {
-                            Spacer()
-                            Text(movie.title)
-                                .font(.system(size: 20))
-                                .bold()
-                                .foregroundColor(.white)
-                                .multilineTextAlignment(.leading)
+                    NavigationLink(destination: DetailView(movie: movie)) {
+                        ZStack {
+                            KFImage(URL(string: movie.backdrop_path))
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .overlay(LinearGradient(gradient: Gradient(colors: [Color.clear, Color.black.opacity(0.35)]), startPoint: .top, endPoint: .bottom))
+                                .clipShape(.rect(cornerRadius: 15))
                             
-                            CircularProgressView(progress: movie.vote_average)
+                            VStack(alignment: .leading, spacing: 10) {
+                                Spacer()
+                                Text(movie.title)
+                                    .font(.system(size: 20))
+                                    .bold()
+                                    .foregroundColor(.white)
+                                    .multilineTextAlignment(.leading)
+                                
+                                CircularProgressView(progress: movie.vote_average)
+                                
+                            }
+                            .frame(width: UIScreen.main.bounds.width - 40, height: UIScreen.main.bounds.height / 4 ,alignment: .leading)
+                            .padding(.bottom, 20)
+                            .padding(.leading, 10)
                             
                         }
-                        .frame(width: UIScreen.main.bounds.width - 40, height: UIScreen.main.bounds.height / 4 ,alignment: .leading)
-                        .padding(.bottom, 20)
-                        .padding(.leading, 10)
-                        
                     }
                 }
                 .tag(index)
@@ -47,13 +50,16 @@ struct CardView: View {
         }
         .tabViewStyle(.page(indexDisplayMode: .never))
         .frame(width: UIScreen.main.bounds.width - 30, height: UIScreen.main.bounds.height / 4)
-        .cornerRadius(15)
+        .cornerRadius(10)
         .clipped()
         .onAppear {
             startTimer()
         }
         .onDisappear {
             stopTimer()
+        }
+        .onChange(of: currentPage) { _ in
+            resetTimer()
         }
         HStack(spacing: 8) {
             ForEach(0..<4) { index in
@@ -68,7 +74,7 @@ struct CardView: View {
         .padding(.top, 8)
     }
     
-    func startTimer() {
+    func startTimer() { // 화면 자동 전환
         timer = Timer.scheduledTimer(withTimeInterval: 4.0, repeats: true) { _ in
             withAnimation {
                 currentPage = (currentPage + 1) % 4
@@ -76,9 +82,14 @@ struct CardView: View {
         }
     }
     
-    func stopTimer() {
+    func stopTimer() { // 백그라운드에서 타이머 종료
         timer?.invalidate()
         timer = nil
+    }
+    
+    func resetTimer() { // 사용자가 직접 페이지 이동 시 자동 전환 리셋 -> 4초 대기 시간
+        stopTimer()
+        startTimer()
     }
 }
 
