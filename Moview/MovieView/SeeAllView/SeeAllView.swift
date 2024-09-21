5c11b941d779aac6a26b0a270fa137c7f79c0176
GwanSon <id147369@naver.com>
2024-09-21 17:02:19 +0900

    feat: 영화 더보기 버튼 추가

--- Moview/MovieView/SeeAllView/SeeAllView.swift
+++ Moview/MovieView/SeeAllView/SeeAllView.swift
@@ -6,26 +6,56 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct SeeAllView: View {
+    var category: MovieCategory
+    @ObservedObject var viewModel: SeeAllViewModel
+    @State private var savedMovies: [MovieModel] = []
     
     let columns = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
     
     var body: some View {
         ScrollView(.vertical) {
             LazyVGrid(columns: columns) {
-//                ForEach(viewModel.genreMovies, id: \.id) { movie in
-//                    NavigationLink(destination: DetailView(movie: movie)) {
-//                        MoviePosterView(movie: movie)
-//                            .foregroundColor(.black)
-//                    }
-//                }
+                ForEach(viewModel.movies, id: \.id) { movie in
+                    NavigationLink(destination: DetailView(movie: movie)) {
+                        MoviePosterView(movie: movie)
+                            .foregroundColor(.black)
+                    }
+                }
+            }
+            MoreButton()
+                .refreshable {
+                    await viewModel.addItem(category: category)
+                }
+
+        }
+        .navigationTitle(category.name)
+        .task {
+            if savedMovies.isEmpty {
+                switch category {
+                case .popular:
+                    viewModel.fetchPopularMovie()
+                case .nowPlaying:
+                    viewModel.fetchNowPlaying()
+                }
             }
         }
-//        .navigationTitle(genre.0)
+        .disabled(viewModel.isLoading)
+        .overlay {
+            if viewModel.isLoading {
+                ProgressView()
+                    .scaleEffect(2)
+                    .progressViewStyle(CircularProgressViewStyle(tint: .blue))
+            }
+        }
+        .onDisappear {
+            savedMovies = viewModel.movies
+        }
     }
 }
 
 #Preview {
-    SeeAllView()
+    SeeAllView(category: .nowPlaying, viewModel: SeeAllViewModel(movieService: MovieService()))
 }
