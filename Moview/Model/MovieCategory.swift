e4b31d9e2eddcfa9981ed10b48e71ffab9711446
GwanSon <id147369@naver.com>
2024-09-21 16:59:22 +0900

    feat: 카테고리 이름 추가

--- Moview/Model/MovieCategory.swift
+++ Moview/Model/MovieCategory.swift
@@ -10,4 +10,11 @@ import Foundation
 enum MovieCategory {
     case popular
     case nowPlaying
+    
+    var name: String {
+        switch self {
+        case .popular: return "인기영화"
+        case .nowPlaying: return "현재 상영작"
+        }
+    }
 }
