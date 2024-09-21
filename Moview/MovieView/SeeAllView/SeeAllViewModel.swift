cfb0789d25958b5d98f812286e208f613832e382
GwanSon <id147369@naver.com>
2024-09-21 17:02:26 +0900

    feat: 영화 더보기 기능 추가

--- Moview/MovieView/SeeAllView/SeeAllViewModel.swift
+++ Moview/MovieView/SeeAllView/SeeAllViewModel.swift
@@ -8,6 +8,71 @@
 import Foundation
 
 class SeeAllViewModel: ObservableObject {
+    @Published var movies: [MovieModel] = []
+    @Published var isLoading: Bool = false
     
+    private let movieService: MovieService
+    var page: Int = 1
     
+    init(movieService: MovieService) {
+        self.movieService = movieService
+    }
+    
+    private func fetchMovies(with page: Int, for category: MovieCategory) {
+        isLoading = true
+        
+        let completion: (MovieResponse?) -> Void = { [weak self] response in
+            guard let self = self else { return }
+            guard let results = response?.results else {
+                DispatchQueue.main.async {
+                    self.isLoading = false
+                }
+                return
+            }
+            
+            let movies = results.map { MovieModel(from: $0) }
+            
+            DispatchQueue.main.async {
+                if page == 0 {
+                    self.movies = movies
+                } else {
+                    self.movies.append(contentsOf: movies)
+                }
+                self.isLoading = false
+            }
+        }
+        
+        switch category {
+        case .popular:
+            if page == 0 {
+                movieService.getPopularMovie(completion: completion)
+            } else {
+                movieService.getPopularMovie(page: page, completion: completion)
+            }
+        case .nowPlaying:
+            if page == 0 {
+                movieService.getNowPlayingMovie(completion: completion)
+            } else {
+                movieService.getNowPlayingMovie(page: page, completion: completion)
+            }
+        }
+    }
+    
+    func fetchPopularMovie() {
+        fetchMovies(with: 0, for: .popular)
+    }
+    
+    func fetchNowPlaying() {
+        fetchMovies(with: 0, for: .nowPlaying)
+    }
+    
+    func addItem(category: MovieCategory) {
+        page += 1
+        switch category {
+        case .popular:
+            fetchMovies(with: page, for: .popular)
+        case .nowPlaying:
+            fetchMovies(with: page, for: .nowPlaying)
+        }
+    }
 }
