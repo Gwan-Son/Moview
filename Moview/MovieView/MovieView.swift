9b2e9ccae65a3323aa7f044191b069cd2cd172ac
GwanSon <id147369@naver.com>
2024-09-22 18:12:49 +0900

    feat: Refactoring

--- Moview/MovieView/MovieView.swift
+++ Moview/MovieView/MovieView.swift
@@ -4,7 +4,7 @@
 //
 //  Created by 심관혁 on 9/14/24.
 //
-// TODO: - 로그인, 게시판, 즐겨찾기
+// TODO: - 로그인, 게시판, 즐겨찾기, 개봉예정작
 
 import SwiftUI
 import Kingfisher
@@ -13,27 +13,25 @@ struct MovieView: View {
     @StateObject var viewModel = MovieViewModel(movieService: MovieService())
     
     var body: some View {
-        NavigationStack {
-            ScrollView(.vertical, showsIndicators: false) {
-                VStack {
-                    // 추천 영화
-                    CardView(viewModel: viewModel)
-                    
-                    GenreCardView()
-                    
-                    DividerCategoryView(category: .popular)
-                    
-                    MovieSlideView(viewModel: viewModel, type: 0)
-                        .padding(.bottom, 10)
-                    
-                    DividerCategoryView(category: .nowPlaying)
-                    
-                    MovieSlideView(viewModel: viewModel, type: 1)
-                    
-                    Spacer()
-                }
+        
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack {
+                // 추천 영화
+                CardView(viewModel: viewModel)
+                
+                GenreCardView()
+                
+                DividerCategoryView(category: .popular)
+                
+                MovieSlideView(viewModel: viewModel, type: 0)
+                    .padding(.bottom, 10)
+                
+                DividerCategoryView(category: .nowPlaying)
+                
+                MovieSlideView(viewModel: viewModel, type: 1)
+                
+                Spacer()
             }
-            
         }
         .task {
             viewModel.fetchNowPlaying()
