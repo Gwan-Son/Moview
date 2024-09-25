2b1ccff76be961a78979d1d947409032189c4c97
GwanSon <id147369@naver.com>
2024-09-25 17:21:41 +0900

    feat: firestoreManger EnvironmentKey 추가

--- Moview/Service/Auth/AuthManagerEnvironmentKey.swift
+++ Moview/Service/Auth/AuthManagerEnvironmentKey.swift
@@ -8,14 +8,24 @@
 import Foundation
 import SwiftUI
 
-public struct AuthManagerEnvironmentKey: EnvironmentKey {
+public struct AuthManagerEnvironmentKey: @preconcurrency EnvironmentKey {
     @MainActor
     public static let defaultValue: AuthManager = AuthManager(configuration: .mock(.signInAndOut))
+}
+
+public struct FirestoreManagerEnvironmentKey: @preconcurrency EnvironmentKey {
+    @MainActor
+    public static let defaultValue: FirestoreManager = FirestoreManager()
 }
 
 public extension EnvironmentValues {
     var auth: AuthManager {
         get { self[AuthManagerEnvironmentKey.self] }
         set { self[AuthManagerEnvironmentKey.self] = newValue }
+    }
+    
+    var db: FirestoreManager {
+        get { self[FirestoreManagerEnvironmentKey.self] }
+        set { self[FirestoreManagerEnvironmentKey.self] = newValue }
     }
 }
