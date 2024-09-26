e138f3aa1f14f4a2217d39652ac6cb6a4af94ce4
GwanSon <id147369@naver.com>
2024-09-27 05:48:33 +0900

    feat: FirebaseAuth 관련 코드 추가

--- Moview/MoviewApp.swift
+++ Moview/MoviewApp.swift
@@ -7,14 +7,33 @@
 
 import SwiftUI
 import FirebaseCore
+import FirebaseAuth
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-
-    return true
-  }
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        
+        return true
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Auth.auth().setAPNSToken(deviceToken, type: .prod)
+    }
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification notification: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        if Auth.auth().canHandleNotification(notification) {
+            completionHandler(.noData)
+            return
+        }
+    }
+    
+    func application(_ application: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool {
+        if Auth.auth().canHandle(url) {
+            return true
+        }
+        return false
+    }
 }
 
 @main
@@ -24,10 +43,8 @@ struct MoviewApp: App {
     var body: some Scene {
         WindowGroup {
             NavigationView {
-//                MovieView()
                 LoginView()
                     .environment(\.auth, AuthManager(configuration: .firebase))
-                    .environment(\.db, FirestoreManager())
             }
         }
     }
