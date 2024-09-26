61190721931c131bc13a37d51bb6ae896db757cc
GwanSon <id147369@naver.com>
2024-09-27 05:48:17 +0900

    feat: 잘못된 코드 수정

--- Moview/Service/Auth/AuthManagerEnvironmentKey.swift
+++ Moview/Service/Auth/AuthManagerEnvironmentKey.swift
@@ -13,19 +13,9 @@ public struct AuthManagerEnvironmentKey: @preconcurrency EnvironmentKey {
     public static let defaultValue: AuthManager = AuthManager(configuration: .mock(.signInAndOut))
 }
 
-public struct FirestoreManagerEnvironmentKey: @preconcurrency EnvironmentKey {
-    @MainActor
-    public static let defaultValue: FirestoreManager = FirestoreManager()
-}
-
 public extension EnvironmentValues {
     var auth: AuthManager {
         get { self[AuthManagerEnvironmentKey.self] }
         set { self[AuthManagerEnvironmentKey.self] = newValue }
-    }
-    
-    var db: FirestoreManager {
-        get { self[FirestoreManagerEnvironmentKey.self] }
-        set { self[FirestoreManagerEnvironmentKey.self] = newValue }
     }
 }
