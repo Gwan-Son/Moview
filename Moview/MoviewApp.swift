8da201fe78039c702e513e623841b6c09cf12077
GwanSon <id147369@naver.com>
2024-10-15 10:40:26 +0900

    feat: Refactoring

--- Moview/MoviewApp.swift
+++ Moview/MoviewApp.swift
@@ -12,39 +12,24 @@ import FirebaseAuth
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseConfiguration.shared.setLoggerLevel(.min)
         FirebaseApp.configure()
         
         return true
-    }
-    
-    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        Auth.auth().setAPNSToken(deviceToken, type: .prod)
-    }
-    
-    func application(_ application: UIApplication, didReceiveRemoteNotification notification: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
-        if Auth.auth().canHandleNotification(notification) {
-            completionHandler(.noData)
-            return
-        }
-    }
-    
-    func application(_ application: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool {
-        if Auth.auth().canHandle(url) {
-            return true
-        }
-        return false
     }
 }
 
 @main
 struct MoviewApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-    
+    @StateObject private var firestoreManager = FirestoreManager()
+
     var body: some Scene {
         WindowGroup {
             NavigationView {
                 LoginView()
                     .environment(\.auth, AuthManager(configuration: .firebase))
+                    .environmentObject(firestoreManager)
             }
         }
     }
