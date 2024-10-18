5434f1a3ed467697dd0ecb6403481e9c75e4ade5
GwanSon <id147369@naver.com>
2024-10-18 21:31:01 +0900

    feat: LazyVGrid에서 아이디가 중복되는 문제를 수정

--- Moview/FavoriteView/FavoriteViewModel.swift
+++ Moview/FavoriteView/FavoriteViewModel.swift
@@ -8,11 +8,10 @@
 import Foundation
 
 class FavoriteViewModel: ObservableObject {
-    // 즐겨찾기 목록
-    // 파이어스토어에서 load한 데이터들을 MovieModel로 재구성
     @Published var movies: [MovieModel] = []
     
     func loadFavorite(_ data: [UserFavorite]) {
+        movies = []
         for favorite in data {
             movies.append(MovieModel(backdrop_path: "", id: Int(favorite.id!)!, original_language: "", original_title: "", overview: "", popularity: 0, poster_path: favorite.poster_path, release_date: "", title: favorite.title, vote_average: favorite.vote_average, vote_count: 0))
         }
